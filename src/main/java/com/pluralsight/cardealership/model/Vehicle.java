4b7d590e4b91ae688eb362f19997139c3f6672a8
AlanChumsawang <alan.chumsawang@outlook.com>
2024-12-13 08:52:14 -0500

    Created Vehicle Model Class

---
+++ src/main/java/com/pluralsight/cardealership/model/Vehicle.java
@@ -0,0 +1,78 @@
+package com.pluralsight.cardealership.model;
+
+public class Vehicle {
+
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
+    private boolean isSold;
+
+    public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price, boolean isSold) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+        this.isSold = isSold;
+    }
+
+    public int getVin() {
+        return vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return vehicleType;
+    }
+
+    public boolean isSold(){ return isSold; }
+
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vin=" + vin +
+                ", year=" + year +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", vehicleType='" + vehicleType + '\'' +
+                ", color='" + color + '\'' +
+                ", odometer=" + odometer +
+                ", price=" + price +
+                '}';
+    }
+}
