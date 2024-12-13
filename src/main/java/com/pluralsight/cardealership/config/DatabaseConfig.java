f3f4a8be78f0e2d3b120d0486ef650e31456a2d4
AlanChumsawang <alan.chumsawang@outlook.com>
2024-12-13 08:49:50 -0500

    Changed the getConnection method to use JdbcTemplate

--- src/main/java/com/pluralsight/cardealership/config/DatabaseConfig.java
+++ src/main/java/com/pluralsight/cardealership/config/DatabaseConfig.java
@@ -1,35 +1,47 @@
 package com.pluralsight.cardealership.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
+@Configuration
 public class DatabaseConfig {
     private static BasicDataSource dataSource; //object to hold the information required to make a connection to the database
 
     /*
-    This method returns a instance of BasicDataSource.
+    This method returns an instance of BasicDataSource.
     If no instance exist, a new instance is initialized with the provided credentials.
      */
 
     //TODO ADD ERROR HANDLING
-    //ADD THREAD SAFETY?
-    public static BasicDataSource getDataSource(String url, String usr, String password) {
+    //TODO ADD THREAD SAFETY?
+    @Bean
+    public BasicDataSource getDataSource(@Value("${spring.datasource.url}") String url,
+                                         @Value("${spring.datasource.username}") String usr,
+                                         @Value("${spring.datasource.password}") String password) {
         if (dataSource == null) {
             dataSource = new BasicDataSource();
-            dataSource.setUrl(System.getenv(url));
-            dataSource.setUsername(System.getenv(usr));
-            dataSource.setPassword(System.getenv(password));
+            dataSource.setUrl(url);
+            dataSource.setUsername(usr);
+            dataSource.setPassword(password);
         }
         return dataSource;
     }
 
     /*
-    This method returns a connection from BasicDataSource.
+    This method returns a JDBCTemplate which injects the BasicDataSource instance instead of directly calling the
+    getConnection method. We are able to do this because the BasicDataSource instance is a bean and is managed by spring.
+    JdbcTemplate is a spring class that manages the connection to the database. I can use this class like a getConnection
+    method to get a connection to the database.
      */
-    public static Connection getConnection(String url, String usr, String password) throws SQLException {
-        return getDataSource(url, usr, password).getConnection();
+    @Bean
+    public JdbcTemplate getConnection(BasicDataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
 
--- src/test/java/com/pluralsight/cardealership/config/test/DatabaseConfigTest.java
+++ src/test/java/com/pluralsight/cardealership/config/test/DatabaseConfigTest.java
@@ -14,44 +14,43 @@ import static org.junit.jupiter.api.Assertions.*;
 class DatabaseConfigTest {
 
     /**
-     * Test that the getDataSource method returns a BasicDataSource and that the environmental variables are correctly being passed
+     * Test that the getDataSource method returns a BasicDataSource and that the environmental variables are correctly being passed.
      */
     @Test
-    void testGetDataConfigTest() {
-        BasicDataSource dataSource = DatabaseConfig.getDataSource("DB_URL", "DB_USR", "DB_PASS");
-        assertNotNull(dataSource);
-        assertEquals(System.getenv("DB_URL"), dataSource.getUrl());
-        assertEquals(System.getenv("DB_USR"), dataSource.getUsername());
-        assertEquals(System.getenv("DB_PASS"), dataSource.getPassword());
+    void testGetDataSource() {
+        BasicDataSource dataSource = new DatabaseConfig().getDataSource("DB_URL", "DB_USR", "DB_PASS");
+        assertNotNull(dataSource, "DataSource should not be null");
+        assertEquals(System.getenv("DB_URL"), dataSource.getUrl(), "URL should match the environment variable");
+        assertEquals(System.getenv("DB_USR"), dataSource.getUsername(), "Username should match the environment variable");
+        assertEquals(System.getenv("DB_PASS"), dataSource.getPassword(), "Password should match the environment variable");
     }
 
     /**
      * Tests if the getConnection method returns a Connection and ensures that the connection is not closed.
      */
-
     @Test
     void testGetConnection() {
-        try (Connection connection = DatabaseConfig.getConnection("DB_URL", "DB_USR", "DB_PASS")) {
-            assertNotNull(connection);
-            assertFalse(connection.isClosed());
+        try (Connection connection = new DatabaseConfig().getDataSource("DB_URL", "DB_USR", "DB_PASS").getConnection()) {
+            assertNotNull(connection, "Connection should not be null");
+            assertFalse(connection.isClosed(), "Connection should be open");
         } catch (SQLException e) {
-            fail("Connection returned an Exception");
+            fail("Connection returned an Exception: " + e.getMessage());
         }
     }
 
     /**
-     * This method test if we are able to run a query in our database, verifying that we have established a connection
-     * to an existing database
+     * This method tests if we are able to run a query in our database, verifying that we have established a connection
+     * to an existing database.
      */
     @Test
     void testDatabaseConnection() {
-        try (Connection connection = DatabaseConfig.getConnection("DB_URL", "DB_USR", "DB_PASS");
+        try (Connection connection = new DatabaseConfig().getDataSource("DB_URL", "DB_USR", "DB_PASS").getConnection();
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery("SELECT 1")) {
-            assertTrue(resultSet.next());
-            assertEquals(1, resultSet.getInt(1));
+            assertTrue(resultSet.next(), "ResultSet should have at least one row");
+            assertEquals(1, resultSet.getInt(1), "Query should return 1");
         } catch (SQLException e) {
-            fail("Query execution unsuccessful");
+            fail("Query execution unsuccessful: " + e.getMessage());
         }
     }
 }
