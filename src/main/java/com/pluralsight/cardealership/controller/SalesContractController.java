959e0b0c542b33ee756b385f384505f5ec26392c
AlanChumsawang <alan.chumsawang@outlook.com>
2024-12-15 10:38:53 -0500

    Cleaned imports and added comments

--- src/main/java/com/pluralsight/cardealership/controller/SalesContractController.java
+++ src/main/java/com/pluralsight/cardealership/controller/SalesContractController.java
@@ -2,28 +2,45 @@ package com.pluralsight.cardealership.controller;
 
 import com.pluralsight.cardealership.dao.SalesContractDao;
 import com.pluralsight.cardealership.model.SalesContract;
-import com.pluralsight.cardealership.model.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+/*
+Entry point for all salesContract related operations annotated with @RestController so that Spring Boot knows to
+create an instance of this class and map it to the appropriate URL path. The @RequestMapping annotation specifies
+that this controller will handle all requests that start with /sales.
+ */
 @RestController
 @RequestMapping(path= "/sales")
 public class SalesContractController {
     private final SalesContractDao salesContractDao;
 
+    /*
+    SalesContractDao is injected into the constructor using the @Autowired annotation. This is a form of dependency
+    injection that allows Spring Boot to automatically create an instance of SalesContractDao and pass it to the
+    constructor when creating an instance of SalesContractController.
+     */
     @Autowired
     public SalesContractController(SalesContractDao salesContractDao) {
         this.salesContractDao = salesContractDao;
     }
 
+    /*
+   The methods below are CRUD operations for vehicles. Each method is annotated with the appropriate HTTP method
+   Starting with CREATE or POST, READ or GET, UPDATE or PUT, and DELETE.
+    */
     @GetMapping
     @ResponseStatus(code = HttpStatus.OK)
-    public List<SalesContract> findAll() {
-        return salesContractDao.findAllSalesContracts();
+    public List<SalesContract> findById(@RequestParam(required = false) Integer id) {
+        List<SalesContract> salesContracts = salesContractDao.findAllSalesContracts();
+        return salesContracts.stream()
+                .filter(sc -> id == null || id.equals(sc.getContractID()))
+                .collect(Collectors.toList());
     }
 
     @PostMapping
@@ -35,21 +52,19 @@ public class SalesContractController {
 
     @PutMapping(path = "/update/{id}")
     @ResponseStatus(code = HttpStatus.OK)
-    public SalesContract updateSalesContract(@PathVariable("vin") Integer oldId, @RequestBody SalesContract updatedContract) {
+    public SalesContract updateSalesContract(@PathVariable("id") Integer oldId, @RequestBody SalesContract updatedContract) {
         SalesContract existingSalesContract = salesContractDao.findSalesContractById(oldId);
         if (existingSalesContract != null) {
             salesContractDao.updateSalesContract(updatedContract);
             return updatedContract;
-
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Vehicle not found");
         }
     }
 
-
     @DeleteMapping(path = "/delete/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void deleteSalesContract(@PathVariable("vin") Integer id) {
+    public void deleteSalesContract(@PathVariable("id") Integer id) {
         salesContractDao.deleteSalesContract(id);
     }
 }
